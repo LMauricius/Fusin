48c22352fd01f8a7b0f14748d68b98bd3d38a39e
Mauricius <mauriciosmit100@gmail.com>
2021-04-08 17:40:15 +0200

    Refactored AnyListener (now IORecorder)

---
+++ Fusin/include/Commands/FusinIORecorder.h
@@ -0,0 +1,66 @@
+#ifndef _FUSIN_ANY_LISTENER_H
+#define _FUSIN_ANY_LISTENER_H
+
+#include "Commands/FusinCommand.h"
+#include "Devices/FusinDeviceEnumerator.h"
+#include "Devices/FusinDevice.h"
+#include <vector>
+#include <map>
+#include <list>
+
+namespace Fusin
+{
+	class _FUSIN_EXPORT IORecorder : public DeviceEnumeratorListener
+	{
+	public:
+		IORecorder(DeviceEnumerator* de = nullptr);
+		~IORecorder();
+
+		void setDeviceEnumerator(DeviceEnumerator* de);
+
+		IOSignal* getStrongestIOSignal() const;
+		Index getStrongestDeviceIndex() const;
+
+		/*
+		Sets the Index of the Device whose signals will be connected, for the specified device type.
+		Use Index 0 is for global devices.
+		*/
+		void setDeviceIndex(Index ind, DeviceType t = DT_ANY);
+		/*
+		Sets the Index of the Device whose signals will be recorded, for the specified device type.
+		Use Index 0 is for global devices.
+		EXAMPLE: setDeviceIndex(1, IO_KEYBOARD | IO_MOUSE) <= Sets the index for keyboard and mouse device inputs to 1
+		*/
+		void setDeviceIndex(Index ind, IOFlags filter);
+		Index getDeviceIndex(DeviceType t = DT_ANY) const;
+		/*
+		Sets the filter that specifies which IOSignals will be recorded.
+		*/
+		void setEnabledInputTypes(IOFlags filter);
+		IOFlags getEnabledInputTypes() const;
+
+	protected:
+		DeviceEnumerator *mDeviceEnumerator;
+		IOFlags mEnabledInputFlags;
+		// Choosen indices to track (not specified indices default to DT_ANY's index)
+		std::map < DeviceType, Index > mDeviceIndices;
+		bool mInvalidated;
+
+		// Actual (device,index) pairs we track, per DeviceType
+		std::map < DeviceType, std::set<std::pair<Device*, Index>> > mDeviceIndexPairSets;
+		IOSignal* mStrongestIOSignal;
+		Index mStrongestDeviceIndex;
+
+		void trackProccess();
+		void untrackProccess();
+		void replug();
+		void replugDeviceType(DeviceType t);
+
+		void deviceRegistered(InputManager* im, Device* d);
+		void deviceUnregistered(InputManager* im, Device* d);
+		void preUpdate(DeviceEnumerator* de);
+		void postUpdate(DeviceEnumerator* de);
+	};
+}
+
+#endif
---
+++ Fusin/src/Commands/FusinIORecorder.cpp
@@ -0,0 +1,189 @@
+#include "Commands/FusinIORecorder.h"
+#include <algorithm>
+
+namespace Fusin
+{
+
+	IORecorder::IORecorder(DeviceEnumerator* de):
+		mInvalidated(false)
+	{
+		mDeviceEnumerator = de;
+		trackProccess();
+
+		mDeviceIndices[DT_ANY] = 0;
+
+		mEnabledInputFlags = IOF_ANY;
+	}
+
+	IORecorder::~IORecorder()
+	{
+		untrackProccess();
+	}
+
+	void IORecorder::setDeviceEnumerator(DeviceEnumerator* de)
+	{
+		untrackProccess();
+		mDeviceEnumerator = de;
+		trackProccess();
+	}
+
+	IOSignal* IORecorder::getStrongestIOSignal() const
+	{
+		return mStrongestIOSignal;
+	}
+
+	Index IORecorder::getStrongestDeviceIndex() const
+	{
+		return mStrongestDeviceIndex;
+	}
+
+
+	void IORecorder::setDeviceIndex(Index ind, DeviceType t)
+	{
+		if (t == DT_ANY)
+		{
+			mDeviceIndices.clear();
+			mDeviceIndices[DT_ANY] = ind;
+		}
+		else
+		{
+			mDeviceIndices[t] = ind;
+		}
+		mInvalidated = true;
+	}
+
+	void IORecorder::setDeviceIndex(Index ind, IOFlags filter)
+	{
+		for (DeviceType devt : ALL_COMPONENT_TYPES)
+		{
+			if (BASE_FLAGS_PER_DEVICE[devt] & filter & IOF_ANY_DEVICE)
+				setDeviceIndex(ind, devt);
+		}
+
+		for (DeviceType devt : ALL_DEVICE_TYPES)
+		{
+			if (BASE_FLAGS_PER_DEVICE[devt] & filter & IOF_ANY_DEVICE)
+				setDeviceIndex(ind, devt);
+		}
+	}
+
+	Index IORecorder::getDeviceIndex(DeviceType t) const
+	{
+		auto it = mDeviceIndices.find(t);
+		if (it == mDeviceIndices.end())
+			return mDeviceIndices.at(DT_ANY);
+		else
+			return it->second;
+	}
+
+	void IORecorder::setEnabledInputTypes(IOFlags filter)
+	{
+		mEnabledInputFlags = filter;
+		mInvalidated = true;
+	}
+
+	IOFlags IORecorder::getEnabledInputTypes() const
+	{
+		return mEnabledInputFlags;
+	}
+
+
+	void IORecorder::trackProccess()
+	{
+		if (mDeviceEnumerator != nullptr)
+		{
+			mDeviceEnumerator->addListener(this);
+			replug();
+		}
+	}
+
+	void IORecorder::untrackProccess()
+	{
+		if (mDeviceEnumerator != nullptr)
+		{
+			mDeviceEnumerator->removeListener(this);
+			replug();
+		}
+	}
+
+	void IORecorder::replug()
+	{
+		mDeviceIndexPairSets.clear();
+		for (DeviceType t : ALL_DEVICE_TYPES)
+		{
+			replugDeviceType(t);
+		}
+		mInvalidated = false;
+	}
+	
+	void IORecorder::replugDeviceType(DeviceType t)
+	{
+		// if device enabled
+		if (mEnabledInputFlags & BASE_FLAGS_PER_DEVICE[t] & IOF_ANY_DEVICE)
+		{
+			Index ind = getDeviceIndex(t);
+			if (ind == 0)
+			{
+				// add all devices
+				for (size_t i = 1; i <= mDeviceEnumerator->maxDeviceIndex(t); i++)
+				{
+					Device* d = mDeviceEnumerator->getDevice(t, i);
+					if (d)
+					{
+						mDeviceIndexPairSets[t].insert({d, i});
+					}
+				}
+			}
+			else
+			{
+				// add device with index ind
+				Device* d = mDeviceEnumerator->getDevice(t, ind);
+				if (d)
+				{
+					mDeviceIndexPairSets[t].insert({d, ind});
+				}
+			}
+		}
+	}
+
+	void IORecorder::deviceRegistered(InputManager* im, Device* d)
+	{
+		replugDeviceType(d->type());
+	}
+
+	void IORecorder::deviceUnregistered(InputManager* im, Device* d)
+	{
+		replugDeviceType(d->type());
+	}
+
+	void IORecorder::preUpdate(DeviceEnumerator* de)
+	{
+		mStrongestIOSignal = nullptr;
+		mStrongestDeviceIndex = -1;
+	}
+
+	void IORecorder::postUpdate(DeviceEnumerator* de)
+	{
+		float maxDis = 0.0f;
+		
+		if (mInvalidated)
+		{
+			replug();
+		}
+
+		for (auto& typeSetPair : mDeviceIndexPairSets)
+		{
+			for (auto& devIndexPair : typeSetPair.second)
+			{
+				IOSignal *s = devIndexPair.first->getStrongestIOSignal(mEnabledInputFlags);
+				if (s->distance() > maxDis)
+				{
+					maxDis = s->distance();
+					mStrongestIOSignal = s;
+					mStrongestDeviceIndex = devIndexPair.second;
+				}
+			}
+		}
+	}
+
+}
